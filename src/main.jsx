21dbcb7e89be0bfc498498cdea52399c92b21b86
rjbelleza <rjbelleza19@gmail.com>
2025-03-09 15:50:44 +0800

    Setup routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" />
+      </Routes>
+    </Router>
   </StrictMode>,
 )
